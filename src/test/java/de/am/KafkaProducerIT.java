68308beb1e761b4777c0fa0d57c1d4836be1c7ce
asmaier <asmaier@web.de>
2016-07-01 13:55:21 +0200

    Updated integration test example to Kafka 0.10.0. The test uses the new producer and consumer APIs of Kafka.

--- src/test/java/de/am/KafkaProducerIT.java
+++ src/test/java/de/am/KafkaProducerIT.java
@@ -1,106 +1,110 @@
 package de.am;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import org.I0Itec.zkclient.ZkClient;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.Test;
 import kafka.admin.TopicCommand;
-import kafka.consumer.ConsumerConfig;
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.KafkaStream;
-import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.producer.KeyedMessage;
-import kafka.producer.Producer;
-import kafka.producer.ProducerConfig;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServer;
 import kafka.utils.MockTime;
 import kafka.utils.TestUtils;
-import kafka.utils.TestZKUtils;
 import kafka.utils.Time;
 import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
 import kafka.zk.EmbeddedZookeeper;
 import static org.junit.Assert.*;
+
 
 /**
  * For online documentation
  * see
- * https://github.com/apache/kafka/blob/0.8.2/core/src/test/scala/unit/kafka/utils/TestUtils.scala
- * https://github.com/apache/kafka/blob/0.8.2/core/src/main/scala/kafka/admin/TopicCommand.scala
- * https://github.com/apache/kafka/blob/0.8.2/core/src/test/scala/unit/kafka/admin/TopicCommandTest.scala
+ * https://github.com/apache/kafka/blob/0.10.0/core/src/test/scala/unit/kafka/utils/TestUtils.scala
+ * https://github.com/apache/kafka/blob/0.10.0/core/src/main/scala/kafka/utils/ZkUtils.scala
+ * https://github.com/apache/kafka/blob/0.10.0/core/src/main/scala/kafka/admin/TopicCommand.scala
+ * https://github.com/apache/kafka/blob/0.10.0/core/src/test/scala/unit/kafka/admin/TopicCommandTest.scala
+ * http://kafka.apache.org/0100/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
+ * http://kafka.apache.org/0100/javadoc/index.html?org/apache/kafka/clients/consumer/KafkaConsumer.html
+ * http://kafka.apache.org/0100/javadoc/org/apache/kafka/clients/consumer/ConsumerRecords.html
+ * http://kafka.apache.org/0100/javadoc/org/apache/kafka/clients/consumer/ConsumerRecord.html
+ * http://kafka.apache.org/0100/javadoc/org/apache/kafka/clients/producer/ProducerRecord.html
  */
 public class KafkaProducerIT {
 
-    private int brokerId = 0;
-    private String topic = "test";
+    private static final String ZKHOST = "127.0.0.1";
+    private static final String BROKERHOST = "127.0.0.1";
+    private static final String BROKERPORT = "9092";
+    private static final String TOPIC = "test";
 
     @Test
-    public void producerTest() throws InterruptedException {
+    public void producerTest() throws InterruptedException, IOException {
 
         // setup Zookeeper
-        String zkConnect = TestZKUtils.zookeeperConnect();
-        EmbeddedZookeeper zkServer = new EmbeddedZookeeper(zkConnect);
-        ZkClient zkClient = new ZkClient(zkServer.connectString(), 30000, 30000, ZKStringSerializer$.MODULE$);
+        EmbeddedZookeeper zkServer = new EmbeddedZookeeper();
+        String zkConnect = ZKHOST + ":" + zkServer.port();
+        ZkClient zkClient = new ZkClient(zkConnect, 30000, 30000, ZKStringSerializer$.MODULE$);
+        ZkUtils zkUtils = ZkUtils.apply(zkClient, false);
 
         // setup Broker
-        int port = TestUtils.choosePort();
-        Properties props = TestUtils.createBrokerConfig(brokerId, port, true);
-
-        KafkaConfig config = new KafkaConfig(props);
+        Properties brokerProps = new Properties();
+        brokerProps.setProperty("zookeeper.connect", zkConnect);
+        brokerProps.setProperty("broker.id", "0");
+        brokerProps.setProperty("log.dirs", Files.createTempDirectory("kafka-").toAbsolutePath().toString());
+        brokerProps.setProperty("listeners", "PLAINTEXT://" + BROKERHOST +":" + BROKERPORT);
+        KafkaConfig config = new KafkaConfig(brokerProps);
         Time mock = new MockTime();
         KafkaServer kafkaServer = TestUtils.createServer(config, mock);
 
-        String [] arguments = new String[]{"--topic", topic, "--partitions", "1","--replication-factor", "1"};
         // create topic
-        TopicCommand.createTopic(zkClient, new TopicCommand.TopicCommandOptions(arguments));
-
-        List<KafkaServer> servers = new ArrayList<KafkaServer>();
+        String [] arguments = new String[]{"--topic", TOPIC, "--partitions", "1","--replication-factor", "1"};
+        TopicCommand.createTopic(zkUtils, new TopicCommand.TopicCommandOptions(arguments));
+        List<KafkaServer> servers = new ArrayList<>();
         servers.add(kafkaServer);
-        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(servers), topic, 0, 5000);
+        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(servers), TOPIC, 0, 5000);
 
         // setup producer
-        Properties properties = TestUtils.getProducerConfig("localhost:" + port);
-        ProducerConfig producerConfig = new ProducerConfig(properties);
-        Producer producer = new Producer(producerConfig);
+        Properties producerProps = new Properties();
+        producerProps.setProperty("bootstrap.servers", BROKERHOST + ":" + BROKERPORT);
+        producerProps.setProperty("key.serializer","org.apache.kafka.common.serialization.IntegerSerializer");
+        producerProps.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        KafkaProducer<Integer, byte[]> producer = new KafkaProducer<Integer, byte[]>(producerProps);
 
-        // setup simple consumer
-        Properties consumerProperties = TestUtils.createConsumerProperties(zkServer.connectString(), "group0", "consumer0", -1);
-        ConsumerConnector consumer = kafka.consumer.Consumer.createJavaConsumerConnector(new ConsumerConfig(consumerProperties));
+        // setup consumer
+        Properties consumerProps = new Properties();
+        consumerProps.setProperty("bootstrap.servers", BROKERHOST + ":" + BROKERPORT);
+        consumerProps.setProperty("group.id", "group0");
+        consumerProps.setProperty("client.id", "consumer0");
+        consumerProps.setProperty("key.deserializer","org.apache.kafka.common.serialization.IntegerDeserializer");
+        consumerProps.setProperty("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+        consumerProps.put("auto.offset.reset", "earliest");  // to make sure the consumer starts from the beginning of the topic
+        KafkaConsumer<Integer, byte[]> consumer = new KafkaConsumer<>(consumerProps);
+        consumer.subscribe(Arrays.asList(TOPIC));
 
         // send message
-        KeyedMessage<Integer, byte[]> data = new KeyedMessage(topic, "test-message".getBytes(StandardCharsets.UTF_8));
-
-        List<KeyedMessage> messages = new ArrayList<KeyedMessage>();
-        messages.add(data);
-
-        producer.send(scala.collection.JavaConversions.asScalaBuffer(messages));
+        ProducerRecord<Integer, byte[]> data = new ProducerRecord<>(TOPIC, 42, "test-message".getBytes(StandardCharsets.UTF_8));
+        producer.send(data);
         producer.close();
 
-        // deleting zookeeper information to make sure the consumer starts from the beginning
-        // see https://stackoverflow.com/questions/14935755/how-to-get-data-from-old-offset-point-in-kafka
-        zkClient.delete("/consumers/group0");
-
         // starting consumer
-        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-        topicCountMap.put(topic, 1);
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-        KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
-        ConsumerIterator<byte[], byte[]> iterator = stream.iterator();
+        ConsumerRecords<Integer, byte[]> records = consumer.poll(3000);
+        assertEquals(1, records.count());
+        Iterator<ConsumerRecord<Integer, byte[]>> recordIterator = records.iterator();
+        ConsumerRecord<Integer, byte[]> record = recordIterator.next();
+        System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+        assertEquals(42, (int) record.key());
+        assertEquals("test-message", new String(record.value(), StandardCharsets.UTF_8));
 
-        if(iterator.hasNext()) {
-            String msg = new String(iterator.next().message(), StandardCharsets.UTF_8);
-            System.out.println(msg);
-            assertEquals("test-message", msg);
-        } else {
-            fail();
-        }
-
-        // cleanup
-        consumer.shutdown();
         kafkaServer.shutdown();
         zkClient.close();
         zkServer.shutdown();
